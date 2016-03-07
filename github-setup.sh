3b3247e6abb4981317b1e835ae073a0328416486
pavel-r <pavel-r@users.noreply.github.com>
2016-03-07 17:24:49 +0100

    Create github-setup.sh

---
+++ github-setup.sh
@@ -0,0 +1,19 @@
+echo -n "Enter your name and press [ENTER]: "
+read name
+echo -n "Enter your email and press [ENTER]: "
+read email
+
+# TODO Check if github_rsa already exist
+ssh-keygen -t rsa -b 4096 -C "$email" -N "" -f $HOME/.ssh/github_rsa
+# Add key to ssh agent
+eval "$(ssh-agent -s)"
+ssh-add $HOME/.ssh/github_rsa
+
+# Configure git
+git config --global user.name "$name"
+git config --global user.email "$email"
+
+# Update config file
+echo Host github.com >> $HOME/.ssh/config
+# Test connecting to github
+ssh -T git@github.com
