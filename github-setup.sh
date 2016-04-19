74742bb64abc2087d21e8302696f026eedcf4286
pavel-r <pavel-r@users.noreply.github.com>
2016-04-19 11:38:06 +0200

    Fix git lg alias

--- github-setup.sh
+++ github-setup.sh
@@ -15,7 +15,7 @@ git config --global user.email "$email"
 git config --global core.editor "vim"
 git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
-git config --global alias.lg "!git lg1"
+git config --global alias.lg "!(git lg1)"
 
 # Update .ssh/config file
 echo Host github.com >> $HOME/.ssh/config
