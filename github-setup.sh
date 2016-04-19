e5579bf6428620b63d6a90562e53e27c14bb3b37
pavel-r <pavel-r@users.noreply.github.com>
2016-04-19 11:33:31 +0200

    Configure git with core editor = vim

--- github-setup.sh
+++ github-setup.sh
@@ -12,6 +12,7 @@ ssh-add $HOME/.ssh/github_rsa
 # Configure git
 git config --global user.name "$name"
 git config --global user.email "$email"
+git config --global core.editor "vim"
 git config --global alias.lg1 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.lg2 "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 git config --global alias.lg "!git lg1"
