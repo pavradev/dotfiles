54cec6ab412b7f23c0499e0f5479c1c64306bc29
pavradev <pavradev@users.noreply.github.com>
2018-01-07 15:52:50 +0100

    Update java-setup.sh

--- java-setup.sh
+++ java-setup.sh
@@ -3,6 +3,7 @@
 # Setup java
 mkdir -p ~/local/java && cd ~/local/java
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz
+tar xzvf jdk-8u141-linux-x64.tar.gz
 echo 'export JAVA_HOME="/home/ubuntu/local/java/jdk1.8.0_141"' >> ~/.bash_profile
 
 # Setup maven
